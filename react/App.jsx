edfb604c02028a314020b1c5fd42ba4e7449539a
Uzair Shamim <uzashamim@gmail.com>
2024-06-20 22:18:47 -0400

    Fix loading

--- react/App.jsx
+++ react/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-// import './App.css';
+import React, { useState } from 'react';
+import './App.css';
 import '../core/page-block-renderer';
 
 const initialItems = [
--- react/index.jsx
+++ react/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './App.jsx';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('react-root'));
+const root = createRoot(document.getElementById('react-root'));
 root.render(<App />);
